6266b3bd0106129f2de3a338b345614c12a63c3e
T.J. Trimble <trimblet@me.com>
2018-05-05 07:18:08 -0700

    Run all tests option

--- tests/run.sh
+++ tests/run.sh
@@ -1,8 +1,9 @@
 # Check if received an argument
 if (( "$#" != 1 )) ; then
-    echo "usage: $0 test.txt"
-    exit 1
+    #echo "usage: $0 test.txt"
+    #exit 1
+    cat *txt | ace -g ../engvec.dat -f 1>tests.out 2>info.out
+else
+    sed '/^[[:blank:]]*#/d;s/#.*//' $1 | sed '/^\s*$/d' | ace -g ../engvec.dat -f 1>tests.out 2>info.out
 fi
-
-sed '/^[[:blank:]]*#/d;s/#.*//' $1 | sed '/^\s*$/d' | ace -g ../engvec.dat -f 1> tests.out 2>info.out
 egrep "parsed \\d+ / \\d+" info.out
